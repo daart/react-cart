e64aa5bc31a025f24d33ae3f3eedb0047b38a2f1
Artem Kuzko <akuzko@corevalue.net>
2017-08-12 18:05:07 +0300

    started refactoring phase

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,250 +1,239 @@
 import React, { Component } from 'react';
 
-
 class Root extends Component {
 	state = {
 		items: [
 			{
-				"id": 1,
-				"title": "headphones",
-				"price": 15.65,
-				"tPrice": 15.65,
-				"inStock": true,
-				"qty": 1
+				id: ''+ Math.random() * 10,
+				title: '42" sony samartTV',
+				qty: 1,
+				price: 499.98
 			},
 			{
-				"id": 2,
-				"title": "Samsung smartTV 42'",
-				"price": 499.98,
-				"tPrice": 499.98,
-				"inStock": true,
-				"qty": 1
+				id: ''+ Math.random() * 10,
+				title: 'haedset Phillips',
+				qty: 2,
+				price: 49.95
 			},
 			{
-				"id": 3,
-				"title": "PS4",
-				"price": 423.77,
-				"tPrice": 423.77,
-				"inStock": true,
-				"qty": 1
+				id: ''+ Math.random() * 10,
+				title: 'mcbook pro 13" retina (2017)',
+				qty: 1,
+				price: 1399.99
 			}
-		],
-
-		totalPrice: 0,
-		totalItemsCnt: 0,
-		newPrice: 0,
-		newItemTitle: ''
-
+		]
 	};
 
-	calcItemTotals(id, operator) {
-
-		this.setState((state, props) => {
-			let { items } = state;
-
-			items.map(item => {
-				if(item.id === id) {
-					if(operator === '+') {
-						item.tPrice += item.price;
-						item.qty += 1;
-					} 
-
-					if(operator === '-') {
-						if(item.tPrice > item.price) {
-							item.tPrice -= item.price;
-							item.qty -= 1;
+	quantifier(id, dx) {
+		this.setState({
+			items: this.state.items.map(item => {
+					if(item.id === id) {
+						if(item.qty >= 1) {
+							item.qty += dx;
 						} 
-						else {
-
+						if(item.qty === 0) {
+							item.qty = 1;
 						}
 					}
-
+					return item; 
 				}
-				return item;
-			})
-
-			return {
-				items
-			}
-		})
-
-	}
-
-	formHandler(e) {
-		e.preventDefault();
-
-		if(!this.state.newPrice || !this.state.newItemTitle) return
-			
-		this.addNewItem();
-	}
-
-	addNewItem() {
-		this.setState({
-			items: [...this.state.items, { 
-				id: '' + Math.random() * 10, 
-				tPrice: this.state.newPrice, 
-				inStock: true,
-				price: this.state.newPrice,
-				title: this.state.newItemTitle  
-			}],
-
-			newPrice: 0,
-			newItemTitle: ''
-		})
-	}
-
-	newPriceHandler(e) {
-		this.setState({ newPrice: isNumeric(e.target.value) ? +e.target.value : 0});
-	}
-
-	newTitleHandler(e) {
-		this.setState({ newItemTitle: e.target.value })
+			)
+		});
 	}
 
 	removeItem(id) {
 		this.setState({
-			items: this.state.items.filter(item => item.id !== id)
-		})
+			items: this.state.items.filter(item => item.id !== id) 
+		});
 	}
 
 	render() {
+		let { items } = this.state;
 
-		let { items, totalPrice, totalItemsCnt } = this.state;
+		let totalItems = items.reduce((prev, next) => {
+			return +prev + +next.qty;
+		}, 0);
 		
-		let priceTotal = items.map(item => item.tPrice).reduce((prev, next) => prev + next);
-		let qtyTotal = items.map(item => item.qty).reduce((prev, next) => prev + next);
+		let totalPrice = items.reduce((prev, next) => {
+			return +(prev + (next.price * next.qty)).toFixed(2);
+		}, 0)
 		
 		console.log(this.state);
 
 		return (
 			<div className="l_wrapper">
+				<AddItemForm />
 				
-				<AddNewForm
-					newPrice={ this.state.newPrice }
-					newItemTitle={ this.state.newItemTitle }
-					newPriceHandler={this.newPriceHandler.bind(this)}
-					newTitleHandler={this.newTitleHandler.bind(this)} 
-					formHandler={this.formHandler.bind(this)}
-				/>
-
-				<div className="items_list">
+				<div className="l_product-list">
 					<ul>
 						{ items.map(item => (
-							<Product 
-								removeItem={ this.removeItem.bind(this) }
-								calcItemTotals={this.calcItemTotals.bind(this)} 
-								key={item.id} 
-								item={item}
-							/>
-						))}
-
+								<li 
+									key={ item.id }
+								>
+									<Product 
+										quantifier={ this.quantifier.bind(this) }
+										item={ item } 
+									/>	
+								</li>
+							))
+						}
 					</ul>
 				</div>
-
+				
 				<Totals 
-					priceTotal={ priceTotal } 
-					qtyTotal={ qtyTotal } 
+					tPrice={ totalPrice }
+					tItemsCnt={ totalItems }
 				/>
 
 			</div>
-		);
+		)
 	}
-
 }
 
-const isNumeric = (n) => !isNaN(parseFloat(n) && isFinite(n));
+class AddItemForm extends Component {
+	state = {
+		title: 'unnamed',
+		qty: 0,
+		price: 0
+	}
 
-const Product = ({ item, calcItemTotals, removeItem }) => {
+	titleHandler(e) {
+		this.setState({
+			title: e.target.value
+		});		
+	}
+
+	priceHandler(e) {
+		this.setState({
+			price: e.target.value
+		});		
+	}
+
+	qtyHandler(e) {
+		this.setState({
+			qty: e.target.value
+		});		
+	}
+
+	addNewItem(formData) {
+		// this.setState({
+		// 	items: [
+		// 	...this.state.items, 
+		// 	{ 
+		// 		formData.qty, formData.price, formData.title 
+		// 	}]
+		// })
+	}
+
+	formHandler(e) {
+		e.preventDefault();
+		let { price, title, qty } = this.state;
+		let formData = { price, title, qty };
+
+		this.addNewItem(formData);
+	}
+	
+
+	render() {
+		console.log(this.state);
+
+		return (
+			<div className="l_form-container">
+				<form 
+					onSubmit={() => console.log('submit')}
+				>
+					<fieldset>
+						<label htmlFor="">
+							Title: 
+							<input 
+								name="title"
+								type="text"
+								onChange={ this.titleHandler.bind(this) }
+								value={ this.state.title }
+							/>
+						</label>
+					</fieldset>
+					<fieldset>
+						<label htmlFor="">
+							Price: 
+							<input 
+								name="price"
+								type="number"
+								onChange={ this.priceHandler.bind(this) }
+							/>
+						</label>
+					</fieldset>
+					<fieldset>
+						<label htmlFor="">
+							qty: 
+							<input 
+								name="qty"
+								type="number"
+								onChange={ this.qtyHandler.bind(this) }
+							/>
+						</label>
+					</fieldset>
+					<button 
+						type="submit" 
+					>
+						Add item
+					</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+const Product = ({ item, quantifier }) => {
 	return (
-		<li>
-			<div>
-				<storng>#:</storng>{ item.id }
-				<span>{ ' ' + item.title }</span>
-				
+		<div className="l_product-item">
+			<div className="l_item-description">
+				<strong>id# :</strong>
+				{ ' ' }
+				<span>
+					{ item.id }
+				</span>
+				{ ' ' }
+				<span>{ item.title }</span>
+			</div>
+			
+			<div className="l_item-controlls">
+				<button 
+					onClick={() => quantifier(item.id, +1)}
+				>
+					+
+				</button>
+				{ ' ' }
+				<strong>{ item.qty }</strong>
+				{ ' ' }
+				<button 
+					onClick={() => quantifier(item.id, -1)}
+				>
+					-
+				</button>
+
 				{ ' ' }
 
 				<button 
-					type="button"
-					onClick={() => removeItem(item.id) }
+					onClick={() => console.log('remove')}
 				>
 					X
 				</button>
-				
-				<div className="incDec_group">
-					<button 
-						id="inc"
-						onClick={() => calcItemTotals(item.id, '+')}
-					>
-						+
-					</button>
-
-					<span>{ item.qty }</span>
-					
-					<button
-						id="dec"
-						onClick={() => calcItemTotals(item.id, '-')}
-					>
-						-
-					</button>
-				</div>
-
-
 			</div>
-		</li>
+
+		</div>
 	)
 }
 
-const Totals = ({ priceTotal, qtyTotal }) => (
-	<div className="totals_panel">
-		<div>Total Price : <strong>{ priceTotal }</strong></div>
-		<div>Total Items Count: <span>{ qtyTotal }</span></div>
-	</div>
-)
-
-const AddNewForm = ({ formHandler, newTitleHandler, newPriceHandler, newPrice, newItemTitle }) => {
+const Totals = ({ tPrice, tItemsCnt }) => {
 	return (
-		<div className="create-item">
-			<form onSubmit={(e) => {
-					formHandler(e);
-				
-					console.log(e)}
-				}
-			>
-				<fieldset>
-					<label htmlFor="">
-						Title : 
-						<input 
-							type="text"
-							onChange={(e) => {
-								newTitleHandler(e);
-							}}
-							value={ newItemTitle }
-						/>
-
-					</label>
-
-					<label htmlFor="">
-						Price : 
-						<input 
-							type="text"
-							onChange={(e) => {
-									newPriceHandler(e);
-								}
-							}
-							value={ newPrice }
-						/>
-
-					</label>
-
-				</fieldset>
-				
-				<button 
-					type="submit"
-				>
-					Add
-				</button>
-
-			</form>
+		<div className="l_totals-footer">
+			<div className="l_t-price">
+				Total Price : <strong>{ tPrice }</strong> 
+			</div>
+			<div className="l_t-items">
+				Total Items : <strong>{ tItemsCnt }</strong> 
+			</div>
 		</div>
 	)
 }
